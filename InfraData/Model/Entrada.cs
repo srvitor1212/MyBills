4e9cec59529fab8eb66f9cc7b7f6e735b0570a56
Vitor - PC <vitor.email2011@gmail.com>
2023-08-08 13:55:04 -0300

    Data annotarion, validações do form

--- InfraData/Model/Entrada.cs
+++ InfraData/Model/Entrada.cs
@@ -4,7 +4,8 @@ namespace SharedProject.Model
 {
     public class Entrada : Base
     {
-        [Required (ErrorMessage = "Nome não foi informado")]
+        [Required (ErrorMessage = "Informe um nome para esse valor por exemplo: Salário, Bônus, Alugel.")]
+        [MinLength (3, ErrorMessage = "Use pelo menos 3 caracteres.")]
         public string Nome { get; set; }
         public double Valor { get; set; }
         public DateTime DataInicio { get; set; }
